177db8bdbe4b2f650fac31603384c694da324b4e
Billy <Nobody2654@yahoo.com>
2012-12-05 16:29:11 -0500

    Fixed some stuff with the bolt/xbow
    Made the xbow only fire exery period of time and the bolt now reliably
    stops on objects. It doesn't stay there once stopped, but....
    Anyway, am I updating too much?

--- Project/Assets/Scripts/HUD-Pause/missileLauncher.js
+++ Project/Assets/Scripts/HUD-Pause/missileLauncher.js
@@ -2,20 +2,22 @@
 //creates the bolt/bullet and fires it
 var projectile : Rigidbody;
 var speed = 20;
-var count = 4;
+var myTimer : float = 2.5;
 
 function Update()
 {
-	if( count <=0)
+	if(myTimer > 0)
+	{
+  		myTimer -= Time.deltaTime;
+ 	}
+	if( myTimer <=0)
 	{
 	 	if( Input.GetButtonDown( "Fire1" ) )
   		{
   			var instantiatedProjectile : Rigidbody = Instantiate(projectile, transform.position, transform.rotation );
   			instantiatedProjectile.velocity = transform.TransformDirection( Vector3( 0, 0, speed ) );
  			Physics.IgnoreCollision( instantiatedProjectile. collider,transform.root.collider );
-  			//plays the animation for the crossbow
-  			animation.Play("Take 001");
-  			count = 4;
+  			myTimer = 2.5;
  		}
  	}
 }
